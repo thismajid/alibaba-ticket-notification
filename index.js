1903353a8ce44e4235fc5964ee2c60b5cafdcf2d
thismajid <mkhorshidian72@gmail.com>
2022-09-15 18:54:17 +0430

    get flight information

--- index.js
+++ index.js
@@ -3,7 +3,34 @@ const axios = require("axios");
 
 const app = express();
 
-setInterval(() => {}, 20000);
+const main = async () => {
+  try {
+    const { data: firstReq } = await axios.post(
+      "https://ws.alibaba.ir/api/v1/flights/domestic/available",
+      {
+        origin: "THR",
+        destination: "TBZ",
+        departureDate: "2022-09-18",
+        adult: 1,
+        child: 0,
+        infant: 0,
+      }
+    );
+    console.log(firstReq);
+    setTimeout(async () => {
+      const { data: secondReq } = await axios.get(
+        `https://ws.alibaba.ir/api/v1/flights/domestic/available/${firstReq?.result?.requestId}`
+      );
+      console.log(secondReq);
+    }, 5000);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+setInterval(async () => {
+  await main();
+}, 20000);
 
 const port = process.env.PORT || 8080;
 
