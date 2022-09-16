af0384e603193aaa2d6a722d5afeeedeffaf8329
thismajid <mkhorshidian72@gmail.com>
2022-09-16 09:59:27 +0430

    add readline to get input from user

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
 const express = require("express");
 const axios = require("axios");
+const readline = require("readline-sync");
 
 const app = express();
 
+const getRequestIdUrl =
+  "https://ws.alibaba.ir/api/v1/flights/domestic/available";
+const getFlightInfoUrl =
+  "https://ws.alibaba.ir/api/v1/flights/domestic/available";
+
+(async () => {})();
+
 const main = async () => {
   try {
-    const { data: firstReq } = await axios.post(
-      "https://ws.alibaba.ir/api/v1/flights/domestic/available",
-      {
-        origin: "THR",
-        destination: "TBZ",
-        departureDate: "2022-09-18",
-        adult: 1,
-        child: 0,
-        infant: 0,
-      }
-    );
+    const { data: firstReq } = await axios.post(getRequestIdUrl, {
+      origin: "THR",
+      destination: "TBZ",
+      departureDate: "2022-09-18",
+      adult: 1,
+      child: 0,
+      infant: 0,
+    });
     console.log(firstReq);
     setTimeout(async () => {
       const { data: secondReq } = await axios.get(
-        `https://ws.alibaba.ir/api/v1/flights/domestic/available/${firstReq?.result?.requestId}`
+        `${getFlightInfoUrl}/${firstReq?.result?.requestId}`
       );
       console.log(secondReq);
-    }, 5000);
+    }, 2500);
   } catch (e) {
     console.log(e);
   }
