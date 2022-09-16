d5ce10c74ca5b219614d4d991ff98762b66764b5
thismajid <mkhorshidian72@gmail.com>
2022-09-16 10:30:35 +0430

    create request body

--- index.js
+++ index.js
@@ -18,16 +18,9 @@ app.listen(port, () => {
 
 const getInput = (text) => readline.question(text);
 
-const main = async () => {
+const main = async (body) => {
   try {
-    const { data: firstReq } = await axios.post(getRequestIdUrl, {
-      origin: "THR",
-      destination: "TBZ",
-      departureDate: "2022-09-18",
-      adult: 1,
-      child: 0,
-      infant: 0,
-    });
+    const { data: firstReq } = await axios.post(getRequestIdUrl, body);
     console.log(firstReq);
     setTimeout(async () => {
       const { data: secondReq } = await axios.get(
@@ -47,14 +40,28 @@ const validationDateFormat = (date) => {
 };
 
 (async () => {
-  const date = await getInput(
+  const departureDate = await getInput(
     "Enter your desired date with this format (yyyy-mm-dd) : "
   );
-  if (!validationDateFormat(date)) {
+  if (!validationDateFormat(departureDate)) {
     console.log("Please enter date with correct format.");
     process.exit(1);
   }
+  const origin = await getInput("Enter your origin : ");
+  const destination = await getInput("Enter your destination : ");
+  const adult = +(await getInput("Enter your adult count : ")) || 1;
+  const child = +(await getInput("Enter your child count : ")) || 0;
+  const infant = +(await getInput("Enter your infant count : ")) || 0;
+  const body = {
+    origin,
+    destination,
+    departureDate,
+    adult,
+    child,
+    infant,
+  };
+
   setInterval(async () => {
-    await main();
+    await main(body);
   }, 20000);
 })();
