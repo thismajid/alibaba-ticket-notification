78f532f75cdf44fe24b4a1b3383b4958a9b7e1fb
thismajid <mkhorshidian72@gmail.com>
2022-09-17 11:52:02 +0430

    change getInput to module type

--- tools.js
+++ tools.js
@@ -1,4 +1,14 @@
+const readline = require("readline-sync");
 const chalk = require("chalk");
+
+exports.getInput = (text, color) => {
+  switch (color) {
+    case "red":
+      return readline.question(chalk.red(text));
+    case "blue":
+      return readline.question(chalk.blue(text));
+  }
+};
 
 exports.logger = (text, color) => {
   switch (color) {
